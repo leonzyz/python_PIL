1fcef33186c40a86ee181e03f65417088449e4c6
leonzyz <leonzyz@ubuntu.ubuntu-domain>
2016-04-23 21:38:23 +0800

    python image processing project

---
+++ test1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+#from PIL import Image
+#from pylab import *
+import os
+print "hello world"
+
+def test_func(a,b):
+    return a+b
+
+print "3+5=%d \n" % test_func(3,5)
+print "os name:%s \n" % os.name
+
+#pil_im= Image.open('hamalong.jpg').convert('L')
+#pil_im= Image.open('hamalong.jpg')
+#im= array(pil_im)
+#imshow(im)
+#show()
+
